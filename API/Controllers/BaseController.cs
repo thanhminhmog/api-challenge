b919f46ef2b64db03aae5c68c3b909acf705ecd1
Ho Tran Minh Quang (FHM.EMU) <QuangHTM@fsoft.com.vn>
2020-03-11 09:36:13 +0700

    Removing unused functions and configs

--- API/Controllers/BaseController.cs
+++ API/Controllers/BaseController.cs
@@ -12,23 +12,6 @@ namespace API
     [ApiController]
     public class BaseController : ControllerBase
     {
-        [HttpPut]
-        public async Task UploadFileToS3(IFormFile file)
-        {
-            using var client = new AmazonS3Client("AKIAQLZICJBWITKRC64V", "IuUWgplycGxkSBmuJUcNPi7PN9/HdpVu0/n50G2/", RegionEndpoint.APSoutheast1);
-            using var newMemoryStream = new MemoryStream();
-            file.CopyTo(newMemoryStream);
-
-            var uploadRequest = new TransferUtilityUploadRequest
-            {
-                InputStream = newMemoryStream,
-                Key = file.FileName,
-                BucketName = "api-challenge-dev",
-                CannedACL = S3CannedACL.PublicRead
-            };
-
-            var fileTransferUtility = new TransferUtility(client);
-            await fileTransferUtility.UploadAsync(uploadRequest);
-        }
+        
     }
 }
